4e19b4f4912cf72d740fd9086a6f7d06d8597433
avcourt <andrew.vcourt@gmail.com>
2019-11-20 00:13:34 -0600

    Use full res images in README for easy right click saving

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm contact-sheet.jpg README.md 2>/dev/null
+mv hi_res/* . 2>/dev/null
+rm -rf thumbs hi_res 2>/dev/null
---
+++ wallamaker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RES=400 # max thumb resolution, if you are running out of memory and
+        # mogrify or montage is failing, try lowering this value.
+
+echo "Checking directory structure."
+if [ -d hi_res ] && [ -d thumbs ]; then
+    echo "It looks like you have run wallamaker.sh before."
+    echo "Ignoring images already in hi_res/"
+    echo "Note: NEW photos should be placed in the directory root, not hi_res/."
+    rm -v contact-sheet.jpg
+    echo "Updating thumbnails with new images..."
+    echo "Generating new thumbnails with max dimension $RES pixels"
+    mogrify -format jpg -path thumbs -thumbnail "$RES"x"$RES" *.{gif,jpeg,jpg,png} 2>/dev/null
+    mv -v *.{gif,jpeg,jpg,png} hi_res 2>/dev/null
+else
+    echo "Creating hi_res/ and /thumb/ directories"
+    mkdir hi_res thumbs
+    mv *.{gif,jpeg,jpg,png} hi_res 2>/dev/null
+    echo "Generating thumbnails with max dimension $RES pixels"
+    mogrify -format jpg -path thumbs -thumbnail "$RES"x"$RES" hi_res/*
+fi
+
+echo "Generating contact sheet"
+montage thumbs/* contact-sheet.jpg
+
+echo "Generating README"
+echo "![contact sheet](contact-sheet.jpg)" >> README.md
+for image in hi_res/*
+do
+    echo "### $image}" >> README.md
+    echo "![$image]($image)" >> README.md
+    echo "***" >> README.md
+done
